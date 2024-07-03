5cb90dddf6928c5f101392535290fcbab53476cd
Prem Codezee <prem.codezee.gmail.com>
2024-07-03 12:52:02 +0530

    Static in express and error handling

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("./public"));
 
 // middleware
 // app.use((req, res, next) => {
 //   console.log("middleware");
 //   next();
+// });
+// dynamic routes
+// app.get("/about/:name", (req, res) => {
+//   res.send(req.params.name + ": he is very good person");
 // });
 
 // static routes
@@ -27,10 +32,14 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-// dynamic routes
-// app.get("/about/:name", (req, res) => {
-//   res.send(req.params.name + ": he is very good person");
-// });
+// error handler
+app.use(function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render("error", { error: err });
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
